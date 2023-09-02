1dd5e69d2ffffa212bf02bf4da8e61c7a0cd47af
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-02 13:09:33 +0900

    postsコントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
+  resources :posts, only: :index
   resources :users, only: [:edit, :update]
 end
