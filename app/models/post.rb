568f438f968670a9cbbe7274ff7e1976b4af709e
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-04 11:07:56 +0900

    ActiveHashの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :target_age
+
+  validates :target_age_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
---
+++ app/models/target_age.rb
@@ -0,0 +1,19 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '3ヶ月〜' },
+    { id: 3, name: '4ヶ月〜' },
+    { id: 4, name: '5ヶ月〜' },
+    { id: 5, name: '6ヶ月〜' },
+    { id: 6, name: '7ヶ月〜' },
+    { id: 7, name: '8ヶ月〜' },
+    { id: 8, name: '9ヶ月〜' },
+    { id: 9, name: '10ヶ月〜' },
+    { id: 10, name: '11ヶ月〜' },
+    { id: 10, name: '12ヶ月〜' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :posts
+
+  end
