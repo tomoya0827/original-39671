fdc18bb4fa87ec4bf9ca973b5960519e1a6efa5c
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-04 11:19:45 +0900

    createアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,22 @@ class PostsController < ApplicationController
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :explanatory_text, :target_age_id)
   end
 
 end
