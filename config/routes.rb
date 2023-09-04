2bbb0ad03ed7ca7f07aa40f41ec05c2848a4cc04
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-04 10:31:25 +0900

    ルーティングにcreateアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
-  resources :posts, only: [:index, :new]
+  resources :posts, only: [:index, :new, :create]
   resources :users, only: [:edit, :update]
 end
