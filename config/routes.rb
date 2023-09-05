4aaf9600631b9b711cbd26b25f05192df6060dc0
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-05 11:33:12 +0900

    投稿削除機能の設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
-  resources :posts, only: [:index, :new, :create, :show]
+  resources :posts, only: [:index, :new, :create, :show, :destroy]
   resources :users, only: [:edit, :update]
 end
