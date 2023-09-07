284d0bc6761d5de3f9f8df22c752b2b03e7c010c
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-07 17:44:58 +0900

    文字数のバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ApplicationRecord
   belongs_to :target_age
 
   validates :image, presence: true
-  validates :explanatory_text, presence: true
+  validates :explanatory_text, presence: true, length: { maximum: 100 }
   validates :target_age_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
