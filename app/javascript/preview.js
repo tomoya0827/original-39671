27090e6ac386a768f8f73526808195c07c6a9215
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-11 16:01:09 +0900

    画像プレビューの設定

---
+++ app/javascript/preview.js
@@ -0,0 +1,29 @@
+document.addEventListener('turbo:load', function(){
+  const postForm = document.getElementById('new_post');
+
+  const previewList = document.getElementById('previews');
+
+  if (postForm) return null;
+
+  const fileField = document.querySelector('input[type="file"][name="post[image]"]');
+  fileField.addEventListener('change', function(e){
+
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image d-block mx-auto');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.e
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+pin "preview", to: "preview.js"
