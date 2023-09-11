1b2b8283f86ebde82788ad27e03e76747064194c
tomoya0827 <140814716+tomoya0827@users.noreply.github.com>
2023-09-11 14:02:53 +0900

    postモデルの単体テストコードの実施

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :post do
-    
+    explanatory_text{Faker::Lorem.sentence}
+    target_age_id{Faker::Number.between(from: 2, to: 11)}
+    association :user
+
+    after(:build) do |post|
+      file_path = Rails.root.join('app', 'assets', 'images', 'test_image.png')
+      post.image.attach(io: File.open(file_path), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '新規投稿' do
+    context '新規投稿できる場合' do
+      it "全ての値が存在すれば投稿できる" do
+        expect(@post).to be_valid
+      end
+    end
+    context '新規投稿できない場合' do
+      it "画像が空では投稿できない" do
+        @post.image = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it "対象年齢が空では投稿できない" do
+        @post.target_age_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Target age can't be blank")
+      end
+
+      it "おすすめ理由が空では投稿できない" do
+        @post.explanatory_text = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Explanatory text can't be blank")
+      end
+
+      it "おすすめ理由が100字を超えては投稿できない" do
+        @post.explanatory_text = Faker::Lorem.sentence(word_count: 101)
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Explanatory text is too long (maximum is 100 characters)")
+      end
+
+      it 'userが紐付いていなければ投稿できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
